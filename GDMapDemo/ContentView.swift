a08ffbcaddab193e14df0e80e47ab01d11539ca9
zhangping <1032460585@qq.com>
2020-04-17 23:54:57 +0800

    Initial Commit

---
+++ GDMapDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GDMapDemo
+//
+//  Created by 张平 on 2020/4/17.
+//  Copyright © 2020 CN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

0c2cf111c9b4cd2188683b46033ca4902af1883f
zp <melenom@163.com>
2020-04-18 15:49:30 +0800

    添加了一点简单的视图，demo更好看了

--- GDMapDemo/ContentView.swift
+++ GDMapDemo/ContentView.swift
@@ -11,8 +11,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        MapView()
-            .frame(width: 400, height: 500, alignment: .center)
+        ZStack {
+            MapView()
+            
+            VStack{
+                ZStack {
+                    Text("背景就是一个地图，当前没有给它太多参数")
+                        .foregroundColor(.pink)
+                        .padding()
+                        .frame(width: UIScreen.main.bounds.width - 10, height: UIScreen.main.bounds.height / 2 , alignment: .center)
+                        .background(LinearGradient(gradient: Gradient(colors: [.blue,.white]), startPoint: .top, endPoint: .bottom))
+                        .cornerRadius(12)
+                       
+                    Rectangle()
+                        .opacity(0.1)
+                        .frame(width: 50, height: 5)
+                        .cornerRadius(2.5)
+                        .offset( y: -160)
+                }
+                .offset( y: 300)
+                .shadow(color: .black, radius: 35, x: 1, y: 1)
+               
+            }
+        }
+            
     }
     
 }

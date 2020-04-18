b7a7546028731f09e0a691f00d370dc46c773541
zp <melenom@163.com>
2020-04-18 14:10:24 +0800

    Revert "Revert "1.0""
    This reverts commit 36c887a4e8c36da700823a6eaf22fb1d3747e65c.

--- GDMapDemo/ContentView.swift
+++ GDMapDemo/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        MapView()
+            .frame(width: 400, height: 500, alignment: .center)
     }
 }
 
--- GDMapDemo/SceneDelegate.swift
+++ GDMapDemo/SceneDelegate.swift
@@ -62,3 +62,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+
+
